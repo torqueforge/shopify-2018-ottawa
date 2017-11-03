d58fe339e54b3c22ee7faa2aca0aafca04468fc2
Sandi Metz <sandi@sandimetz.com>
2017-11-03 03:47:31 -0400

    Delete obsolete code

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -20,20 +20,6 @@ class HousePhrases
 end
 
 class House
-  DATA =
-    [ "the horse and the hound and the horn that belonged to",
-      "the farmer sowing his corn that kept",
-      "the rooster that crowed in the morn that woke",
-      "the priest all shaven and shorn that married",
-      "the man all tattered and torn that kissed",
-      "the maiden all forlorn that milked",
-      "the cow with the crumpled horn that tossed",
-      "the dog that worried",
-      "the cat that killed",
-      "the rat that ate",
-      "the malt that lay in",
-      "the house that Jack built"]
-
   attr_reader :data, :prefix
   def initialize(phrases: HousePhrases.new, prefixer: MundanePrefixer.new)
     @data = phrases.data
