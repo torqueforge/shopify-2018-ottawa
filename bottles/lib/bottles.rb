cc9ad0303460be73077e11005ca130ba71c22722
Sandi Metz <sandi@sandimetz.com>
2017-11-03 01:52:23 -0400

    Copy Bottles to DescendingVerseSong

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -5,6 +5,26 @@ class Integer
 end
 
 
+class DescendingVerseSong
+  attr_reader :verse_template
+
+  def initialize(verse_template: BottleVerse)
+    @verse_template = verse_template
+  end
+
+  def song
+    verses(99, 0)
+  end
+
+  def verses(upper, lower)
+    upper.downto(lower).map { |i| verse(i) }.join("\n")
+  end
+
+  def verse(number)
+    verse_template.new(number).lyrics
+  end
+end
+
 class Bottles
   attr_reader :verse_template
   def initialize(verse_template: BottleVerse)
