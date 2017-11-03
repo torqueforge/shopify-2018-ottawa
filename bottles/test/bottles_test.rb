24293b85ebd0d301f9c66c728bf85007cf74d1bf
Sandi Metz <sandi@sandimetz.com>
2017-11-03 02:02:35 -0400

    Delete obsolete code

--- bottles/test/bottles_test.rb
+++ bottles/test/bottles_test.rb
@@ -63,19 +63,6 @@ class DescendingVerseSongTest < Minitest::Test
     assert_equal expected, DescendingVerseSong.new(verse_template: VerseDouble).verses(99, 98)
   end
 
-  def test_a_couple_verses
-    expected = "99 bottles of beer on the wall, " +
-      "99 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of beer on the wall.\n" +
-      "\n" +
-      "98 bottles of beer on the wall, " +
-      "98 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "97 bottles of beer on the wall.\n"
-    assert_equal expected, DescendingVerseSong.new.verses(99, 98)
-  end
-
   def test_a_few_verses
     expected = "2 bottles of beer on the wall, " +
       "2 bottles of beer.\n" +
