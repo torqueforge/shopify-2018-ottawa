a564dd3765bbeb951c0f6eeacc4a7f3cc18a0455
Sandi Metz <sandi@sandimetz.com>
2017-11-03 02:38:58 -0400

    Pass parameter in 1 case

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -14,7 +14,7 @@ class House
     def line(num)
       case num
       when 1
-        "This is #{phrase}the house that Jack built.\n"
+        "This is #{phrase(num)}the house that Jack built.\n"
       when 2
         "This is the malt that lay in the house that Jack built.\n"
       when 3
